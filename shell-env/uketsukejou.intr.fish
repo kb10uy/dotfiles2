31efc2fa75deb267d602e2b690d040cc16195830
Yū Kobayashi <kb10uy@kb10uy.org>
2023-09-25 19:28:00 +0900

    Apply sherry

--- shell-env/uketsukejou.intr.fish
+++
@@ -1,8 +0,0 @@
-# Load interactive environments and completions
-source /home/kb10uy/.phpbrew/phpbrew.fish
-
-# Interactive operation settings
-set -x GPG_TTY (tty)
-if [ -n "$SSH_CONNECTION" ]
-  set -x PINENTRY_USER_DATA 'USE_CURSES=1'
-end
